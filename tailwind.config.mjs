2040be1779a820f8ded626076bc5d6dbc15027c6
Niraj <niraj.lama@ie.com.au>
2024-03-28 09:50:13 +1100

    feat: Add more variants and config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        crimson: ['Crimson Text', 'serif'],
+      },
       colors: {
         ieBrand1: "#CCf2F7",
         ieBrand2: "#99E5EE",
@@ -12,7 +15,29 @@ export default {
         gray: {
           50: "#FAFAFC",
           100: "#E9E9EC",
+          300: "#ACAEB6",
+          400: "#92959F",
+          500: "#777C87",
+          600: "#5D6370",
+          700: "#434959",
+          800: "#293041",
+          900: "#0f172a",
         },
+      },
+      grayscale: {
+        0: '0',
+        DEFAULT: '100%',
+      },
+      blur: {
+        0: '0',
+        none: '0',
+        sm: '4px',
+        DEFAULT: '8px',
+        md: '12px',
+        lg: '16px',
+        xl: '24px',
+        '2xl': '40px',
+        '3xl': '64px',
       },
     },
   },
